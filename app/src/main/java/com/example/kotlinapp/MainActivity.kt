9572846b759ddf3b8d4c3a492eb3c8c99909c6f6
Ivan Savitsky <isavitsky17@gmail.com>
2024-07-11 22:57:28 +0700

    implement serialization of the only one pokemon

--- app/src/main/java/com/example/kotlinapp/MainActivity.kt
+++ app/src/main/java/com/example/kotlinapp/MainActivity.kt
@@ -1,20 +1,57 @@
 package com.example.kotlinapp
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.widget.TextView
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
+import java.io.IOException
+import android.util.Log
+import android.net.Uri
+import org.json.JSONObject
+import androidx.core.net.toUri
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val textView = findViewById<TextView>(R.id.response)
+
+        val client = OkHttpClient()
+        val url = "https://pokeapi.co/api/v2/pokemon?limit=1"
+        val request = Request.Builder()
+            .url(url)
+            .build()
+        try {
+            client.newCall(request).enqueue(object : Callback {
+                override fun onFailure(call: Call, e: IOException) {
+                    e.printStackTrace()
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+                    if (response.isSuccessful) {
+                        runOnUiThread {
+                            val json = JSONObject(response.body!!.string())
+                            val results = json.getJSONArray("results")
+                            val name = results.getJSONObject(0).getString("name")
+
+                            val pokemon = Pokemon(name)
+                            textView.text = pokemon.name
+                        }
+                    }
+                }
+            })
+        } catch (e: IOException) {
+            println("Ошибка подключения: $e");
         }
     }
 }
+
+class Pokemon(
+    val name: String
+)
+
